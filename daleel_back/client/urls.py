9e0314e31c542ae43abd8f8e8b3015852fd5d1b6
Mohamed Hossam <80326979+mhdhossam@users.noreply.github.com>
2024-12-18 02:09:47 +0200

    update

--- daleel_back/client/urls.py
+++ daleel_back/client/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
-    TokenObtainPairView,
+   
 )
 from .views import VendorRegisterView, CustomerRegisterView, CustomTokenObtainPairView
 # from rest_framework.routers import DefaultRouter
@@ -21,7 +21,7 @@ urlpatterns = [
 
     path('api/auth/register/vendor/', VendorRegisterView.as_view(), name='vendor-register'),
     path('api/auth/register/customer/', CustomerRegisterView.as_view(), name='customer-register'),
-    path('api/auth/login/', TokenObtainPairView.as_view(), name='TokenObtainPairView'),
+    path('api/auth/login/', CustomTokenObtainPairView.as_view(), name='CustomTokenObtainPairView'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
