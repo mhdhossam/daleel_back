d3172fbc9f190f96f107f946acd8a2c8ea03de9b
Mohamed salah <109218485+MOsalahCS@users.noreply.github.com>
2024-12-22 00:22:26 +0200

    update

--- daleel_back/store/urls.py
+++ daleel_back/store/urls.py
@@ -4,6 +4,8 @@ from.views import AddToCartView,ViewCartView,RemoveFromCartView
 
 urlpatterns = [
 
+
+
 path('api/store/createorder',AddToCartView.as_view(),name='add-to-cart'),
 path('api/cart/remove/<int:pk>/', RemoveFromCartView.as_view(), name='remove-from-cart'),
 path('api/store/vieworder',ViewCartView.as_view(),name='view-order')
